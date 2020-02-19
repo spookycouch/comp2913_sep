e8039c68c16a9fd25644ebb26b4d386cfeac511a
Sam Barnes <sc18sjb@leeds.ac.uk>
2020-02-19 13:34:28 +0000

    implementation: iter 1 initialize web file structure

---
+++ src/web_app/app.js
@@ -0,0 +1,33 @@
+// Port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Packages
+var express = require('express');
+var app = express();
+var path = require('path');
+const request = require('request');
+
+// Components
+var css = require('./scaffholding/css');
+
+app.use(express.static('src'));  // Used to make the resources accessible (css etc.)
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+
+    
+
+    res.render(path.join(__dirname + '/views/pages/index.ejs'),
+    {
+        title: "home"
+    });
+});
+
+
+
+
+app.listen(port, hostname, function() {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
