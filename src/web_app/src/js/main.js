d534a85fb82238bfd299376e91a261ad1e4c4ada
Sam Barnes <sc18sjb@leeds.ac.uk>
2020-02-20 13:48:40 +0000

    implementation: iter 1 side navigation bar on mobile devices]

--- src/web_app/src/js/main.js
+++ src/web_app/src/js/main.js
@@ -5,10 +5,24 @@ $(document).ready(function() {
         e.preventDefault();
         hamburgerPressed = !hamburgerPressed;
 
-        if (hamburgerPressed) 
-            $('.hamburger').addClass('is-active');
-        else
+        if (hamburgerPressed) {
+            $('.hamburger').addClass('is-active')
+
+            $('.header__dropdown').css('display', 'block').animate({
+                backgroundColor: 'rgba(0, 0, 0, .6)'
+            })
+            $('.dropdown__container').addClass('dropdown__container--open');
+
+        } else {
             $('.hamburger').removeClass('is-active');
+            $('.header__dropdown').animate({
+                backgroundColor: 'rgba(0, 0, 0, 0)'
+            }, function() {
+                $('.header__dropdown').css('display', 'none');
+            });
+            $('.dropdown__container').removeClass('dropdown__container--open');
+        }
+
 
     });
 });
