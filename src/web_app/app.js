aa4d59cd09cc4b09af37dc501310a1605d11f31c
Artyom Tiunelis <sc18at@leeds.ac.uk>
2020-02-22 19:11:35 +0000

    Added routes

--- src/web_app/app.js
+++ src/web_app/app.js
@@ -6,6 +6,7 @@ const port = 3000;
 var express = require('express');
 var app = express();
 var path = require('path');
+var routes = require('./routes/index');
 const request = require('request');
 var user = require('./modules/user.js');
 
@@ -14,55 +15,11 @@ var user = require('./modules/user.js');
 const webname = ' The Edgy ';
 
 // Components
-
-
 app.use(express.static('src'));  // Used to make the resources accessible (css etc.)
+app.use('/',routes);
 app.set('view engine', 'ejs');
 
 
-// Routes
-app.get(['/', '/home'], function(req, res) {
-    
-    //user.registerUser("Diego Calanzone", "diegocalanzone@gmail.com", "123", "123", "Leeds", "Leeds", 1582394167);
-    user.loginUser("john.d@mail.com", "123").then(function(user){
-
-        console.log(user);
-
-        res.render(path.join(__dirname + '/views/pages/index.ejs'),
-        {
-            title: webname + "| Home"
-        });
-    }).catch(function(error){});
-});
-
-app.get('/login', function(req, res) {
-    res.render(path.join(__dirname + '/views/pages/login.ejs'),
-    {
-        title: webname + "| Login"
-    });
-});
-
-app.get('/memberships', function(req, res) {
-    res.render(path.join(__dirname + '/views/pages/memberships.ejs'),
-    {
-        title: webname + "| Memberships"
-    });
-});
-
-
-app.get('/facilities', function(req, res) {
-    res.render(path.join(__dirname + '/views/pages/facilities.ejs'),
-    {
-        title: webname + "| Facilities"
-    });
-});
-
-app.get('/contact', function(req, res) {
-    res.render(path.join(__dirname + '/views/pages/contact.ejs'),
-    {
-        title: webname + "| Contact"
-    });
-});
 
 
 // Run the app
