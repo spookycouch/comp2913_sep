058c29ebf1a96129f6b449c412129bdc8fbae876
Artyom Tiunelis <sc18at@leeds.ac.uk>
2020-03-02 18:37:15 +0000

    basic login functionality

--- src/web_app/routes/userRoutes.js
+++ src/web_app/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const validation = require('../validation');
+const validation = require('./validation');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 var userDB = require('../modules/user');
@@ -27,6 +28,20 @@ router.post('/register', async function(req, res) {
     }
     
     res.send(user);
+
+});
+
+router.post('/login', async function(req, res){
+
+    //TODO: change fora db validation
+    const value = validation.loginValidation(req.body);
+    
+    if(value.error != undefined){
+        return res.status(400).send(value.error.message)
+    }
+
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token',token).send(token);
 
 });
 
