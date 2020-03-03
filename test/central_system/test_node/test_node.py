8962311af9356a24104a6c06adbd2340df949040
joejeffcock <sc18j3j@leeds.ac.uk>
2020-03-03 01:14:28 +0000

    fix: test_node loads yaml from os abspath

--- test/central_system/test_node/test_node.py
+++ test/central_system/test_node/test_node.py
@@ -2,15 +2,18 @@ import unittest
 import requests
 import json
 import yaml
+import os
 
 HOST = 'http://127.0.0.1:3000'
 HEADERS = {'content-type': 'application/json'}
+YAML_PATH = os.path.dirname(os.path.abspath(__file__)) + '/test_node.yaml'
+
 
 class TestNode(unittest.TestCase):
     @classmethod
     def setUpClass(self):
     # load test yaml
-        self.test_cases = yaml.safe_load(open('test_node.yaml','r'))
+        self.test_cases = yaml.safe_load(open(YAML_PATH,'r'))
 
 
     def test_0_register(self):
@@ -19,6 +22,7 @@ class TestNode(unittest.TestCase):
         for case in cases:
             payload = json.dumps(case['payload'])
             resp = requests.post(url, data=payload, headers=HEADERS)
+            print resp
 
     def test_1_login(self):
         url = HOST + '/user/register'
@@ -26,6 +30,7 @@ class TestNode(unittest.TestCase):
         for case in cases:
             payload = json.dumps(case['payload'])
             resp = requests.post(url, data=payload, headers=HEADERS)
+            print resp
 
     @classmethod
     def tearDownClass(self):
