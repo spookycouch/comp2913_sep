9e6f7ddc18c1e9d6f32d05965c8d9963c545dc8e
Sam Barnes <sc18sjb@leeds.ac.uk>
2020-03-04 18:10:33 +0000

    implementation: iter 2 login page redesign to better fit website style

--- src/web_app/src/js/main.js
+++ src/web_app/src/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    var hamburgerPressed = false
+    var hamburgerPressed = false;
+    var registertoggle = false;
 
     // slick configuration
     $(".reviews__carousel").slick({
@@ -48,4 +49,29 @@ $(document).ready(function() {
             $('.dropdown__container').removeClass('dropdown__container--open');
         }
     });
+
+
+
+    $('.half__form-transition-header').on('click', function(e) {
+
+        // $('#half-2').removeClass('login__half--background');
+        // $('#half-2').addClass('login__half--form');
+
+        // $('#half-1').removeClass('login__half--form');
+        // $('#half-1').addClass('login__half--background');
+        registertoggle = !registertoggle
+
+        if (registertoggle) {
+            $('.login__half--login').css('display', 'none');
+            $('.login__half--register').css('display', 'flex');
+            $(this).find('.form-transition-header__text').text("Login");
+        } else {
+            $('.login__half--login').css('display', 'flex');
+            $('.login__half--register').css('display', 'none');
+            $(this).find('.form-transition-header__text').text("Register");
+
+        }
+        
+        // e.preventDefault();
+    })
 });
