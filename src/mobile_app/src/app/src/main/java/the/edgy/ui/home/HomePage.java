287c828cb0ae814969c2a5575644804b6c31c735
Georgy Gunkin <sc18gg@leeds.ac.uk>
2020-03-09 22:52:34 +0000

    feat: back buttons now update the menu correspondingly

--- src/mobile_app/src/app/src/main/java/the/edgy/ui/home/HomePage.java
+++ src/mobile_app/src/app/src/main/java/the/edgy/ui/home/HomePage.java
@@ -4,8 +4,13 @@ import androidx.annotation.NonNull;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -25,23 +30,30 @@ public class HomePage extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                     Fragment selectedFragment = null;
 
+                    // Tag for menu switching later.
+                    String fragmentTag = null;
+
                     // Id changes with different buttons,
                     // so we can detect which button has been clicked.
                     switch (menuItem.getItemId()) {
                         case R.id.navigation_home:
                             selectedFragment = new HomeFragment();
+                            fragmentTag = "fragment_home";
                             break;
 
                         case R.id.navigation_activities:
                             selectedFragment = new ActivitiesFragment();
+                            fragmentTag = "fragment_activities";
                             break;
 
                         case R.id.navigation_venues:
                             selectedFragment = new VenuesFragment();
+                            fragmentTag = "fragment_venues";
                             break;
 
                         case R.id.navigation_profile:
                             selectedFragment = new ProfileFragment();
+                            fragmentTag = "fragment_profile";
                             break;
                     }
 
@@ -49,7 +61,7 @@ public class HomePage extends AppCompatActivity {
                     // fragment corresponding to the button clicked.
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                             selectedFragment).
-                            addToBackStack(null).
+                            addToBackStack(fragmentTag).
                             commit();
 
                     // Returning true to highlight the clicked navigation button.
@@ -83,12 +95,51 @@ public class HomePage extends AppCompatActivity {
     // If there are no fragments, the behaviour is default, which is going back in activity levels.
     @Override
     public void onBackPressed() {
+        BottomNavigationView bottom_navigation = findViewById(R.id.bottom_navigation);
+
+        Menu navigation_menu = bottom_navigation.getMenu();
+
         if(getSupportFragmentManager().getBackStackEntryCount() == 0) {
             super.onBackPressed();
         }
         else {
             // Popping the current fragment and showing the previous one.
             getSupportFragmentManager().popBackStack();
+
+            int topOfBackStackIndex = getSupportFragmentManager().getBackStackEntryCount() - 2; // -1 for previous fragment on the stack.
+
+            if (topOfBackStackIndex < 0) {
+                // First fragment is always home.
+                navigation_menu.findItem(R.id.navigation_home).setChecked(true);
+                return;
+            }
+
+            FragmentManager.BackStackEntry topOfBackStackFragment = getSupportFragmentManager().
+                    getBackStackEntryAt(topOfBackStackIndex);
+
+            String tag = topOfBackStackFragment.getName();
+
+            if (tag != null) {
+                switch (tag) {
+                    case "fragment_home":
+                        navigation_menu.findItem(R.id.navigation_home).setChecked(true);
+                        break;
+
+                    case "fragment_activities":
+                        navigation_menu.findItem(R.id.navigation_activities).setChecked(true);
+                        break;
+
+                    case "fragment_venues":
+                        navigation_menu.findItem(R.id.navigation_venues).setChecked(true);
+                        break;
+
+                    case "fragment_profile":
+                        navigation_menu.findItem(R.id.navigation_profile).setChecked(true);
+                        break;
+
+                }
+            }
+
         }
     }
 
